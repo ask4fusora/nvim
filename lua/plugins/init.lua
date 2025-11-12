13a40d0e3b463838766101f69c4578e62f894c1a
ask4fusora <ask4fusora+github@gmail.com>
2025-11-12 12:45:44 +0700

    Refactor installation script to seperate modules

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,3 @@
-vim.pack.add({
-  { src = "https://github.com/catppuccin/nvim",                 name = "catppuccin" },
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" },
-})
-
 require("plugins.catppuccin")
 require("plugins.nvim-treesitter")
+require("plugins.fidget")
--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,3 +1,5 @@
+vim.pack.add({ { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "master" } })
+
 require("nvim-treesitter.configs").setup({
   modules = {},
   ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "typst" },
