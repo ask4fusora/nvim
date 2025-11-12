35d470348e8c1409b67f28eedc1574351e41d3fc
ask4fusora <ask4fusora+github@gmail.com>
2025-11-12 17:13:49 +0700

    Use mini.pick git only for git directory

--- lua/plugins/mini-pick.lua
+++ lua/plugins/mini-pick.lua
@@ -4,6 +4,13 @@ local pick = require("mini.pick")
 
 pick.setup()
 
-vim.keymap.set("n", "<C-p>", function() pick.builtin.files({ tool = "git" }) end)
+vim.keymap.set("n", "<C-p>", function()
+  if vim.fn.finddir(".git", vim.fn.getcwd() .. ";") ~= "" then
+    pick.builtin.files({ tool = "git" })
+  else
+    pick.builtin.files()
+  end
+end)
+
 vim.keymap.set("n", "g/", function() pick.builtin.grep_live() end)
 vim.keymap.set("n", "	", function() pick.builtin.buffers() end)
