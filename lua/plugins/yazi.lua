3b081036055fe004cc93b1b7ff0a3c68c5570957
ask4fusora <ask4fusora+github@gmail.com>
2025-11-12 15:35:06 +0700

    Install yazi as file manager plugin

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,17 @@
+vim.pack.add({
+  { src = "https://github.com/nvim-lua/plenary.nvim" },
+  { src = "https://github.com/mikavilpas/yazi.nvim" },
+})
+
+local yazi = require("yazi")
+
+vim.g.loaded_netrwPlugin = 1
+vim.api.nvim_create_autocmd("UIEnter", {
+  callback = function()
+    yazi.setup({
+      open_for_directories = true,
+    })
+  end,
+})
+
+vim.keymap.set("n", "<C-E>", function() yazi.yazi() end)
