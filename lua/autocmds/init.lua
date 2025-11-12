935889353692a23750e00a241144059ad9873b95
ask4fusora <ask4fusora+github@gmail.com>
2025-11-12 16:21:50 +0700

    Autocmd for return cursor position post yank

--- lua/autocmds/init.lua
+++ lua/autocmds/init.lua
@@ -1,5 +1,10 @@
+vim.api.nvim_create_autocmd({ "VimEnter", "CursorMoved" }, {
+  callback = function() vim.g.cursor_position = vim.fn.getpos(".") end
+})
+
 vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function()
+    if vim.v.event.operator == "y" then vim.fn.setpos(".", vim.g.cursor_position) end
     vim.hl.on_yank({ higroup = "Visual", timeout = 233 })
   end
 })
