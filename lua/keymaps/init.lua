a7ac51e722ce786237af167eea6e70a5289c8007
ask4fusora <ask4fusora+github@gmail.com>
2025-11-14 00:46:10 +0700

    Lsp goto keymap

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -6,3 +6,9 @@ vim.keymap.set("n", "] ", function() for _ = 1, vim.v.count1 do vim.fn.append(vi
 vim.keymap.set("n", "[ ", function() for _ = 1, vim.v.count1 do vim.fn.append(vim.fn.line(".") - 1, { "" }) end end)
 
 vim.keymap.set("", "<M-F>", function() vim.lsp.buf.format({ async = true }) end)
+
+vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
+vim.keymap.set("n", "gy", function() vim.lsp.buf.type_definition() end)
+vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end)
+vim.keymap.set("n", "gI", function() vim.lsp.buf.implementation() end)
+vim.keymap.set("n", "gA", function() vim.lsp.buf.references() end)
