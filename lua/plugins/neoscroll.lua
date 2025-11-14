e3dacbd698a502e827981ad0c1b0e77ce2e5fe38
ask4fusora <ask4fusora+github@gmail.com>
2025-11-14 22:54:59 +0700

    Enable smooth scrolling

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -2,5 +2,5 @@ vim.pack.add({ { src = "https://github.com/karb94/neoscroll.nvim" } })
 
 require("neoscroll").setup({
   easing = "quadratic",
-  duration_multiplier = 0.056
+  duration_multiplier = 0.09
 })
