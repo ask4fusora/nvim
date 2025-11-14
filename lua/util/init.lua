746ed4fab95ba988cbb2c9d87878b4917dbbe49c
ask4fusora <ask4fusora+github@gmail.com>
2025-11-14 11:55:59 +0700

    Implement filter function for future use

---
+++ lua/util/init.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+--- Filters a list based on a predicate function.
+---@generic T
+---@param array T[] The list to filter.
+---@param predicate fun(item: T, index: integer): boolean The test function.
+---@return T[] result A new list with only the elements that passed the test.
+M.filter = function(array, predicate)
+  local result = {}
+
+  for i, v in ipairs(array) do
+    if predicate(v, i) then table.insert(result, v) end
+  end
+
+  return result
+end
+
+return M
