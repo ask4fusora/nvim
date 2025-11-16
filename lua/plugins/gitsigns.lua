85ef294edcdd5f69dd3d2bae652cd61c48220b6a
ask4fusora <ask4fusora+github@gmail.com>
2025-11-16 14:27:49 +0700

    Plugin `gitsigns` add keymap to refresh all buffer

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,3 +1,7 @@
 vim.pack.add({ { src = "https://github.com/lewis6991/gitsigns.nvim" } })
 
-require("gitsigns").setup()
+local gitsigns = require("gitsigns")
+
+gitsigns.setup()
+
+vim.keymap.set("n", "<leader>gs", function() gitsigns.refresh() end)
