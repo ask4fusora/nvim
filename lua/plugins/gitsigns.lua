6fd8f28e1c8ce59aa8bd9bac555f01c19ef2b074
ask4fusora <ask4fusora+github@gmail.com>
2025-11-16 14:38:19 +0700

    Added keymap `go_to_hunk`

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,4 +4,12 @@ local gitsigns = require("gitsigns")
 
 gitsigns.setup()
 
+---@param direction 'next' | 'prev'
+local go_to_hunk = function(direction)
+  if vim.wo.diff then return direction and ']c' or '[c' end
+  return gitsigns.nav_hunk(direction)
+end
+
 vim.keymap.set("n", "<leader>gs", function() gitsigns.refresh() end)
+vim.keymap.set('n', ']c', function() go_to_hunk('next') end)
+vim.keymap.set('n', '[c', function() go_to_hunk('prev') end)
