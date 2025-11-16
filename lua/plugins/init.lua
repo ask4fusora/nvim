44c58c1e6c71ea7f50b28238cb94286ef23bac39
ask4fusora <ask4fusora+github@gmail.com>
2025-11-16 23:45:25 +0700

    Disable smooth scroll

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,7 +7,7 @@ vim.defer_fn(
     require("plugins.nvim-treesitter")
     require("plugins.yazi")
     require("plugins.zen-mode")
-    require("plugins.neoscroll")
+    -- require("plugins.neoscroll")
     -- require("plugins.copilot")
     require("plugins.zeta")
     require("plugins.lspkind")
