485e1995fa8a53069afbc636b23c0b254894e692
ask4fusora <ask4fusora+github@gmail.com>
2025-11-24 21:30:43 +0700

    `typst-preview` plugin

--- lua/plugins/nvim-treesitter-context.lua
+++ lua/plugins/nvim-treesitter-context.lua
@@ -2,6 +2,6 @@ vim.pack.add({
   { src = "https://github.com/nvim-treesitter/nvim-treesitter-context" },
 })
 
--- local treesitter_context = require('treesitter-context')
---
--- vim.keymap.set('n', '<leader>k', function() treesitter_context.go_to_context(vim.v.count1) end)
+local treesitter_context = require('treesitter-context')
+
+vim.keymap.set('n', '<leader>k', function() treesitter_context.go_to_context(vim.v.count1) end)
