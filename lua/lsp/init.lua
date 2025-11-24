4438d5607c01ae059461b86d2eababaac9666989
ask4fusora <ask4fusora+github@gmail.com>
2025-11-25 01:28:49 +0700

    Refine lsp document highlight logic

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -56,14 +56,61 @@ local setup_lsp_capabilities = function(client, args)
   if client.server_capabilities.documentHighlightProvider then
     vim.o.updatetime = 89
 
+    local unique_id = tostring(client.id) .. tostring(args.buf)
+
+    local document_highlight_augroup = vim.api.nvim_create_augroup(
+      "DocumentHighlight" .. unique_id,
+      { clear = true }
+    )
+
+    local global_request_id = nil ---@type integer?
+
     vim.api.nvim_create_autocmd({ "CursorHold" }, {
+      group = document_highlight_augroup,
       buffer = args.buf,
-      callback = function() vim.lsp.buf.document_highlight() end
+      callback = function()
+        local params = vim.lsp.util.make_position_params(nil, client.offset_encoding)
+        local local_request_id = nil ---@type integer?
+
+        local status, request_id = client:request(
+          'textDocument/documentHighlight',
+          params,
+          function(err, result, context, config)
+            if global_request_id ~= local_request_id then return end
+
+            if result then
+              vim.lsp.handlers['textDocument/documentHighlight'](err, result, context, config)
+            end
+          end,
+          args.buf
+        )
+
+        if status then
+          global_request_id = request_id
+          local_request_id = request_id
+        end
+      end
     })
 
-    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+    vim.api.nvim_create_autocmd({ "BufLeave", "CursorMoved", "CursorMovedI" }, {
+      group = document_highlight_augroup,
       buffer = args.buf,
-      callback = function() vim.lsp.buf.clear_references() end
+      callback = function()
+        vim.lsp.buf.clear_references()
+        global_request_id = nil
+      end
+    })
+
+    vim.api.nvim_create_autocmd('LspDetach', {
+      group = vim.api.nvim_create_augroup('LspDetach' .. unique_id, { clear = true }),
+      buffer = args.buf,
+      callback = function(lsp_detach_args)
+        if assert(lsp_detach_args.data.client_id) == client.id then
+          global_request_id = nil
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds({ group = document_highlight_augroup, buffer = args.buf })
+        end
+      end
     })
   end
 
