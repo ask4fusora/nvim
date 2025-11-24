d9b097cec2f9b3f341a5a4c0e6cb0ac7b31ddbda
ask4fusora <ask4fusora+github@gmail.com>
2025-11-24 23:10:51 +0700

    Plugin `nvim-treesitter-textobjects`

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,6 +2,7 @@ vim.pack.add({ { src = "https://github.com/nvim-treesitter/nvim-treesitter", ver
 
 require("nvim-treesitter.configs").setup({
   modules = {},
+
   ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "typst" },
   ignore_install = {},
   sync_install = false,
@@ -10,5 +11,26 @@ require("nvim-treesitter.configs").setup({
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
-  }
+  },
+
+
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = false,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+        ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
+      },
+      selection_modes = {
+        ['@parameter.outer'] = 'v',
+        ['@function.outer'] = 'V',
+        ['@class.outer'] = '<c-v>',
+      },
+      include_surrounding_whitespace = false,
+    },
+  },
 })
