b276c4166f0a0ab714d10939caa9fc5555482e65
ask4fusora <ask4fusora+github@gmail.com>
2025-12-09 22:44:29 +0700

    Allow sticky header

--- lua/plugins/nvim-treesitter-context.lua
+++ lua/plugins/nvim-treesitter-context.lua
@@ -6,7 +6,6 @@ local treesitter_context = require('treesitter-context')
 
 treesitter_context.setup({
   enable = true,
-  max_lines = 1
 })
 
 vim.keymap.set('n', '<leader>k', function() treesitter_context.go_to_context(vim.v.count1) end)
